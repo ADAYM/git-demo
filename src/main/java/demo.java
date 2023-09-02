b454b5570275505dbca6b1cb5ce7a21338f83a5c
liuzhou <354108472@qq.com>
2023-09-02 20:13:24 +0800

    hot-fix refresh

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -2,5 +2,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("version 1.0");
         System.out.println("version 1.1");
+        System.out.println("hot-fix refresh");
     }
 }
